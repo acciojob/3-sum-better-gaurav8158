8b1d5b8d25c358d38173d127e6abf8c209f9aa6a
gaurav8158 <120136122+gaurav8158@users.noreply.github.com>
2023-03-22 22:21:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function threeSum(arr, target) {
+	let min = arr[0];
+	let n = arr.length;
+	for(let i=0;i<n;i++){
+		for(let j=i+1;j<n;j++){
+			for(let k=j+1;k<n;k++){
+			let sum = arr[i]+arr[j]+arr[k];
+				if(min>sum){
+					min=sum
+				}
+			}
+		}
+	}
 // write your code here
-  
+  return min;
 }
 
 module.exports = threeSum;
