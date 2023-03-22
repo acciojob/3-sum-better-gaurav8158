0686b1fb1cbb64f12dadf8f5ecd5bf085d860823
gaurav8158 <120136122+gaurav8158@users.noreply.github.com>
2023-03-22 22:33:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function threeSum(arr, target) {
-	let min = arr[0];
-	let n = arr.length;
-	for(let i=0;i<n;i++){
-		for(let j=i+1;j<n;j++){
-			for(let k=j+1;k<n;k++){
-			let sum = arr[i]+arr[j]+arr[k]-target;
-				if(sum>=0){
-					min=arr[i]+arr[j]+arr[k]
-				}
-			}
-		}
-	}
-// write your code here
-  return min;
+	let ans = Infinity;
+  arr.sort((a, b) => a - b);
+  arr.forEach((a, i) => {
+    const target2 = target - a;
+    let l = i + 1;
+    let r = arr.length - 1;
+	  
+    while (l < r) {
+      if (Math.abs(target - (arr[l] + arr[r] + a)) <= Math.abs(target - ans)) {
+        ans = arr[l] + arr[r] + a;
+      }
+      if (arr[l] + arr[r] > target2) r--;
+      else l++;
+    }
+  });
+  return ans;
 }
 
 module.exports = threeSum;
